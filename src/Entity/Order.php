d01e32cdc603d3ac64cd70d061dc8f8a4ed43ff7
AlexandreSama <alexandrehainy12@gmail.com>
2024-02-07 17:43:44 +0100

    v

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -14,13 +14,6 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ApiResource]
-#[ApiResource(
-    operations: [
-        new Patch(),
-        new Get(),
-        new GetCollection()
-    ],
-)]
 #[ORM\Entity(repositoryClass: OrderRepository::class)]
 #[ORM\Table(name: '`order`')]
 class Order
